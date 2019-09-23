8e22345dd4ee9a861c47a057cdf8b5eec4de2c95
Mickael Guene <mickael.guene@st.com>
2019-09-23 16:51:35 +0200

    empty driver with success registry

--- st-vd56g3.c
+++ st-vd56g3.c
@@ -8,7 +8,37 @@
  *
  */
 
+#include <linux/i2c.h>
 #include <linux/module.h>
+
+static int vd56g3_probe(struct i2c_client *client)
+{
+	dev_info(&client->dev, "vd56g3 probe successfully");
+
+	return 0;
+}
+
+static int vd56g3_remove(struct i2c_client *client)
+{
+	return 0;
+}
+
+static const struct of_device_id vd56g3_dt_ids[] = {
+	{ .compatible = "st,st-vd56g3" },
+	{ /* sentinel */ }
+};
+MODULE_DEVICE_TABLE(of, vd56g3_dt_ids);
+
+static struct i2c_driver vd56g3_i2c_driver = {
+	.driver = {
+		.name  = "st-vd56g3",
+		.of_match_table = vd56g3_dt_ids,
+	},
+	.probe_new = vd56g3_probe,
+	.remove = vd56g3_remove,
+};
+
+module_i2c_driver(vd56g3_i2c_driver);
 
 MODULE_AUTHOR("Mickael Guene <mickael.guene@st.com>");
 MODULE_DESCRIPTION("vd56g3 Camera Subdev Driver");
