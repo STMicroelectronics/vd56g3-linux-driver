6108ae1355254f5d35d219d274a6c1cb13d2da0e
Mickael Guene <mickael.guene@st.com>
2020-01-07 12:41:26 +0100

    avoid loosing sd.flags set by v4l2_i2c_subdev_init

--- st-vd56g3.c
+++ st-vd56g3.c
@@ -1615,7 +1615,7 @@ static int vd56g3_probe(struct i2c_client *client)
 	}
 
 	v4l2_i2c_subdev_init(&sensor->sd, client, &vd56g3_subdev_ops);
-	sensor->sd.flags = V4L2_SUBDEV_FL_HAS_DEVNODE;
+	sensor->sd.flags |= V4L2_SUBDEV_FL_HAS_DEVNODE;
 	sensor->pad.flags = MEDIA_PAD_FL_SOURCE;
 	sensor->sd.entity.ops = &vd56g3_subdev_entity_ops;
 	sensor->sd.entity.function = MEDIA_ENT_F_CAM_SENSOR;
